4d26fef970ecf552bfef049c5af8406a058641a0
drystar <chris.drysdale12@gmail.com>
2019-12-13 15:43:08 -0800

    refactoring http auth

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,4 @@
-class Admin::ProductsController < ApplicationController
+class Admin::ProductsController < Admin::AdminController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 
   def index
