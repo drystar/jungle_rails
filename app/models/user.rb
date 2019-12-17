cbefdeecfad8971d261303e7596c2c4db5231d60
drystar <chris.drysdale12@gmail.com>
2019-12-16 23:13:04 -0800

    updating user model and switching spec files to model folder

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, 
+  validates :email_uniqueness: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+
 end
+
