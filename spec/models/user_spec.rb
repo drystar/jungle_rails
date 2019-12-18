ddf872d463bb2de6b0e78bfe5f54b4643271b1a7
drystar <chris.drysdale12@gmail.com>
2019-12-17 19:03:51 -0800

    cleaning up validation specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,65 +1,58 @@
 require 'rails_helper'
 
-RSpec.describe User, type: model do
+RSpec.describe User, type: :model do
+  before :each do @user = User.new(first_name: "Buddy", last_name: "theElf", email: "elf@gmail.com", password: "password", password_confirmation: "password")
+  end
 
   describe 'Validations' do
 
     it "is valid with valid attributes" do
-      @user = User.new(first_name: "Buddy", last_name: "theElf", email: "elf@gmail.com", password: "passwword", password_confirmation: "password")
       expect(@user).to be_valid
-      expect(@user.errors.full_messages).to be_nil
     end
 
     it "is invalid without a password" do
-      @user = User.new(first_name: "Buddy", last_name: "theElf", email: "elf@gmail.com", password: nil, password_confirmation: "password")
+      @user.password = nil
       expect(@user).not_to be_valid
-      expect(@user.errors.full_messages).to be_nil
     end
 
     it "is invalid without a password confirmation" do
-      @user = User.new(first_name: "Buddy", last_name: "theElf", email: "elf@gmail.com", password: "passwword", password_confirmation: nil)
+      @user.password_confirmation = nil
       expect(@user).not_to be_valid
-      expect(@user.errors.full_messages).to include("Password can't be blank")
     end
 
     it "is invalid without password and confirmation password matching" do
-      @user = User.new(first_name: "Buddy", last_name: "theElf", email: "elf@gmail.com", password: "password", password_confirmation: "pa55w0rd")
+      @user.password_confirmation = 123456
       expect(@user).not_to be_valid
-      expect(@user.errors.full_messages).to include("Passwords don't match")
     end
 
     it "is invalid when password is too short" do
-      @user = User.new(first_name: "Buddy", last_name: "theElf", email: "elf@gmail.com", password: "passw", password_confirmation: "passw")
+      @user.password = "passw" 
       expect(@user).not_to be_valid
-      expect(@user.errors.full_messages).to include("Passwords must be at least 6 characters")
     end
 
 
 
-    it "is invalid when email is not unique" do
-      @user = User.new(first_name: "Buddy", last_name: "theElf", email: "copy@gmail.com", password: "password", password_confirmation: "password")
-      expect(@user).not_to be_valid
-      expect(@user.errors.full_messages).to include("Email is not unique")
+    it "is invalid when email is already in use" do
+      @user.save
+      user1 = User.new(first_name: "Buddy", last_name: "theElf", email: "elf@gmail.com", password: "password", password_confirmation: "password")
+      expect(user1).not_to be_valid
     end
 
     it "is invalid without an email" do
-      @user = User.new(first_name: "Buddy", last_name: "theElf", email: nil, password: "password", password_confirmation: "password")
+      @user.email = nil
       expect(@user).not_to be_valid
-      expect(@user.errors.full_messages).to include("Email can't be blank")
     end
 
 
 
     it "is invalid without first name" do
-      @user = User.new(first_name: nil, last_name: "theElf", email: "elf@gmail.com", password: "password", password_confirmation: "password")
+      @user.first_name = nil
       expect(@user).not_to be_valid
-      expect(@user.errors.full_messages).to include("First name can't be blank")
     end
 
     it "is invalid without last name" do
-      @user = User.new(first_name: "Buddy", last_name: nil, email: "elf@gmail.com", password: "password", password_confirmation: "password")
+      @user.last_name = nil
       expect(@user).not_to be_valid
-      expect(@user.errors.full_messages).to include("Last name can't be blank")
     end
 
   end
